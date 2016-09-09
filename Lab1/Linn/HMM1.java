875435a4c0c66a9e8ebdf88ed4adbed3df879090
Linn <bergelid@kth.se>
2016-09-09 15:19:42 +0200

    hmm1 completed

---
+++ Lab1/Linn/HMM1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+public class HMM1 {
+
+	static Scanner in = new Scanner(System.in);
+	static double[][] transMatrix;
+	static double[][] emissionMatrix;
+	static double[][] initState;
+
+
+	public static void main (String[] args) {
+
+		// Create transition matrix
+		int trow = in.nextInt();
+		int tcol = in.nextInt();
+		transMatrix = Matrix.createMatrix(trow, tcol, in);
+
+		// Create emission matrix
+		int erow = in.nextInt();
+		int ecol = in.nextInt();
+		emissionMatrix = Matrix.createMatrix(erow, ecol, in);
+
+		// Create initial state probability matrix
+		int isrow = in.nextInt();
+		int iscol = in.nextInt();
+		initState = Matrix.createMatrix(isrow, iscol, in);
+
+		double[][] nextState = Matrix.multiMatrix(initState, transMatrix);
+		double[][] nextObs = Matrix.multiMatrix(nextState, emissionMatrix);
+
+		System.out.print(nextObs.length + " " + nextObs[0].length + " ");
+		Matrix.printMatrix(nextObs);
+	}
+
+
+
+
+
+
+}
---
+++ Lab1/Linn/Matrix.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Matrix {
+
+
+
+	public static double[][] multiMatrix(double[][] matA, double[][] matB) {
+
+		double[][] res = new double[matA.length][matB[0].length];
+
+		for (int aRow = 0; aRow < matA.length; aRow++) {
+			for (int bCol = 0; bCol < matB[0].length; bCol++) {
+				for (int bRow = 0; bRow < matB.length; bRow++) {
+					res[aRow][bCol] += matA[aRow][bRow]*matB[bRow][bCol];
+				}
+			}
+		}
+		return res;
+	}
+
+	public static double[][] createMatrix(int row, int col, Scanner in) {
+
+		double[][] matrix = new double[row][col];
+
+		for (int r = 0; r < row; r++) {
+			for (int c = 0; c < col; c++) {
+				matrix[r][c] = Double.parseDouble(in.next());
+			}
+		}
+		return matrix;
+	}
+
+	public static void printMatrix(double[][] mtx) {
+
+		for (int i = 0; i < mtx.length; i++) {
+			for (int j = 0; j < mtx[0].length; j++) {
+				System.out.print(mtx[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
+	
